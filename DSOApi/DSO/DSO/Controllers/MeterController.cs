3fe9a6ff5cf1d8bc44489b918eaaad1409b52468
jasondavey <jasondavey@hotmail.com>
2014-11-18 19:04:29 -0800

    Added service infrastructure

---
+++ DSOApi/DSO/DSO/Controllers/MeterController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Http;
+
+namespace DSOApi.Controllers
+{
+    [RoutePrefix("api/")]
+    public class MeterController : ApiController
+    {
+        [Route("BuyCredit")]
+        public void BuyCredit([FromBody] string creditValue, string serialNumber)
+        {
+        }
+    }
+}
